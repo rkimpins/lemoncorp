d62a0591d1cba693bce21520cfbd6b28a5638c37
Randal Kimpinski <rkimpins@ualberta.ca>
2021-07-12 14:51:59 -0600

    Add resume download button and styled it

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import resume_file from '../../files/resume.pdf';
+import classes from './Resume.module.css';
 
 const resume = () => (
-	<embed src={resume_file} type="application/pdf" width="98%" height="800px"  />
 	//Leave a little bit of space around the edges to escape scroll depending on screen size
+	<div className={classes}>
+		<a href={resume_file} download="Randal Kimpinski Resume"><button type="button">Download</button></a>
+		<embed src={resume_file} type="application/pdf" width="98%" height="800px"  />
+	</div>
 );
 
 export default resume;
