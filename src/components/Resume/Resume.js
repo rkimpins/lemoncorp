60d198cbb2bd16ceced183f85c5b15c7c2974553
Randal Kimpinski <rkimpins@ualberta.ca>
2021-07-14 16:00:21 -0600

    Change resume render method to iframe

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,11 +2,14 @@ import React from 'react';
 import resume_file from '../../files/resume.pdf';
 import classes from './Resume.module.css';
 
+import {Document} from 'react-pdf';
+
 const resume = () => (
 	//Leave a little bit of space around the edges to escape scroll depending on screen size
+	//<embed src={resume_file} type="application/pdf" width="98%" height="800px"  />
 	<div className={classes}>
 		<a href={resume_file} download="Randal Kimpinski Resume"><button type="button">Download</button></a>
-		<embed src={resume_file} type="application/pdf" width="98%" height="800px"  />
+		<iframe src={resume_file} title="resume" width="100%" height="800px"/>
 	</div>
 );
 
