fcaf143471c3e15004b54e0807c14155721f5ec4
Randal Kimpinski <rkimpins@ualberta.ca>
2021-07-17 23:03:55 -0600

    Remove _blank vulnerability

--- src/components/Contact/LogoAndText/LogoAndText.js
+++ src/components/Contact/LogoAndText/LogoAndText.js
@@ -5,7 +5,7 @@ import classes from './LogoAndText.module.css';
 const logoAndText = (props) => {
 	return (
 		<div className={classes.LAT}>
-			<a href={props.link} target="_blank">
+			<a href={props.link} target="_blank" rel="noreferrer">
 				<img src={props.logo} alt={props.alt}/>
 			</a>
 			<p>{props.text}</p>
