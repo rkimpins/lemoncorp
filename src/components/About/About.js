aa5448ef31c68983626b96a1d953b7cf6d6aa633
Randal Kimpinski <rkimpins@ualberta.ca>
2021-08-17 13:12:48 -0600

    Update about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
+import image from '../../images/about/me.jpg';
+import classes from './About.module.css';
+
 const about = () => (
-    <p>ABOUT</p>
+    <div className={classes.About}>
+        <img src={image} alt="Randal" />
+        <div className={classes.Paragraph}>
+            <p>
+            Hello friends! My name is Randal and I am a recent computer science graduate.
+            </p>
+            <p>
+            I currently spend my free time searching for work, improving the side-projects
+            that I have created, and trying to stay sane during this pandemic.
+            </p>
+            <p>
+            My favorite areas of computer science are web-dev, database management,
+            data visualization, and machine learning.
+            </p>
+            <p>
+            Outside of compsci, I enjoy rock climbing, swing dancing, biking, and whatever
+            the hobby of the month is. This month is ambigrams.
+            </p>
+            <p>
+            I love exploring new things and learning everything I can along the way.
+            </p>
+        </div>
+    </div>
 )
 
 export default about;
