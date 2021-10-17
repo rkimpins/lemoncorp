b2f09ba1fdfcdc21bd3ccf135bde82d440833c90
Randal Kimpinski <rkimpins@ualberta.ca>
2021-10-17 16:51:52 -0600

    Fixed minor error messages

--- src/components/Home/ProjectCard/ProjectCard.js
+++ src/components/Home/ProjectCard/ProjectCard.js
@@ -6,7 +6,7 @@ const ProjectCard = (props) => (
 	<div className={classes.ProjectCard}>
 		<h3>{props.title}</h3>
 		<p>{props.description}</p>
-		<img src={props.image} />
+		<img src={props.image} alt="Screenshot of {props.title}" />
 	</div>
 );
 
--- src/components/RandomColors/RandomColors.js
+++ src/components/RandomColors/RandomColors.js
@@ -33,9 +33,9 @@ class RandomColors extends Component {
 	}
 
 	arrayIsEqual = (a, b) => {
-		if (a.length != b.length) {return false;}
+		if (a.length !== b.length) {return false;}
 		for (let i = 0; i < a.length; i++) {
-			if (a[i] != b[i]) {return false;}
+			if (a[i] !== b[i]) {return false;}
 		}
 		return true;
 	}
